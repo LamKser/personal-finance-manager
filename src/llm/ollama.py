6f9732bc94399ea526a5496b6ea7934da5388511
LamKser <dinhhoanglam2801118@gmail.com>
2026-07-21 01:26:46 +0700

    add logger

--- src/llm/ollama.py
+++ src/llm/ollama.py
@@ -1,9 +1,15 @@
+from os import getenv
+
 from langchain_ollama import ChatOllama
 
+from src.logger import get_logger
+
+log = get_logger()
 
 class OllamaModel:
     def __init__(self, model_name: str, reasoning: bool | str | None = None):
         self.llm = ChatOllama(model=model_name, reasoning=reasoning)
+        log.info(f"Using Provider: 'Ollama' - Model: '{model_name}' - think_mode: '{reasoning}'")
         
     def get_llm(self):
         return self.llm
---
+++ src/logger.py
@@ -0,0 +1,35 @@
+import logging
+from os import getenv
+import os
+
+def get_logger(level: str = getenv("LOG_LEVEL", "INFO"),
+               name: str = __name__,
+               file_name: str = getenv("LOG_FILE", "logs/app.log")):
+    
+    parent_dir = os.path.dirname(file_name)
+    if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
+        
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+    
+    logger.setLevel(level)
+    logger.propagate = False
+
+    console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(file_name, mode="a", encoding="utf-8")
+    formatter = logging.Formatter(
+        "{asctime} - {levelname} - {message}", # 2025-07-22 15:58 - WARNING - Messages
+        style="{",
+        datefmt="%Y-%m-%d %H:%M",
+    )
+
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    logger.info(f"[LOG]: Logs are saved at '{file_name}'")
+    return logger
