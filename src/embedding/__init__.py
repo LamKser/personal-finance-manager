3c2b18e2d707001b8ad982037cf11abbc69ea798
LamKser <dinhhoanglam2801118@gmail.com>
2026-09-05 00:34:59 +0700

    edit ollama embedding

--- src/embedding/__init__.py
+++ src/embedding/__init__.py
@@ -4,14 +4,19 @@ log = getLogger(__name__)
 
 from src.embedding.ollama import OllamaEmbedding
 
+OLLAMA_DEFAULT_DIMENSION = {
+    "nomic-embed-text-v2-moe:latest": 768,
+    "qwen3-embedding:0.6b": 1024
+}
+
 
 class EmbeddingModel:
     def __init__(self, provider: str, model: str, dimension: int = None) -> None:
         self.provider = provider
         self.model = model
-        self.dimension = dimension
+        self.dimension = dimension if dimension else OLLAMA_DEFAULT_DIMENSION[model]
         if provider == "ollama":
-            self.model = OllamaEmbedding(model, dimension)
+            self.model = OllamaEmbedding(model, self.dimension)
         else:
             log.info("[EMBEDDING] Provider '%s' is not supported", provider)
 
--- src/embedding/ollama.py
+++ src/embedding/ollama.py
@@ -4,19 +4,12 @@ from logging import getLogger
 from langchain_ollama import OllamaEmbeddings
 
 
-DEFAULT_DIMENSION = {
-    "nomic-embed-text-v2-moe:latest": 768,
-    "qwen3-embedding:0.6b": 1024
-}
-
-
 log = getLogger(__name__)
 
 
 class OllamaEmbedding:
-    def __init__(self, model: str, dimension: int = None):
-        self.dimension = dimension if dimension else DEFAULT_DIMENSION[model]
-        self.embedding_model = OllamaEmbeddings(model=model, dimensions=self.dimension)
+    def __init__(self, model: str, dimension: int):
+        self.embedding_model = OllamaEmbeddings(model=model, dimensions=dimension)
         
     def get_embedding(self, text: str) -> List[float]:
         return self.embedding_model.embed_query(text)
