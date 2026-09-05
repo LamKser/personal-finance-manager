3ab5eec75515ae13a0ea39005aa0afc5a21fd0b8
LamKser <dinhhoanglam2801118@gmail.com>
2026-09-05 14:11:06 +0700

    add default threshold for retrieval

--- src/embedding/__init__.py
+++ src/embedding/__init__.py
@@ -5,8 +5,8 @@ log = getLogger(__name__)
 from src.embedding.ollama import OllamaEmbedding
 
 OLLAMA_DEFAULT_DIMENSION = {
-    "nomic-embed-text-v2-moe:latest": 768,
-    "qwen3-embedding:0.6b": 1024
+    "nomic-embed-text-v2-moe:latest": 768, # threshold router: 0.3
+    "qwen3-embedding:0.6b": 1024 # threshold router: 0.4
 }
 
 
