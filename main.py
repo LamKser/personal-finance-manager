d28a40f66648b43cb675e6cb9404d156777c0139
LamKser <dinhhoanglam2801118@gmail.com>
2026-09-11 02:10:43 +0700

    modify main.py to take prompt from terminal

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-from src.logger import logger
-
-from dotenv import load_dotenv
-
-load_dotenv()
-logger()
-
+from argparse import ArgumentParser
 
 from src.agent import LangGraphAgent
+from src.logger import logger
+
+logger()
+
+parser = ArgumentParser(description="Run LangGraph Agent")
+parser.add_argument(
+    "-p",
+    "--prompt",
+    type=str,
+    required=True,
+    help="User prompt to send to the agent",
+)
+
+args = parser.parse_args()
+
 agent = LangGraphAgent()
 
-# from IPython.display import Image, display
-# from src.utils.visualization.graph_visualize import GraphVisualization
-# display(Image(GraphVisualization().visualize_png(agent.graph)))
-
-history = "Hôm nay chi hết 12 triệu cho mua sắm"
-# history = "Xin chào"
-    # "tool Tối Thứ Ba mua cà phê ở Ministop, trả tiền mặt 18 nghìn."
-    # "(tool) Đếm số giao dịch của Tháng 2"    
-    # "Đếm toàn bộ số lượng giao dịch"
-    # "Xin chào"
-a = agent.invoke_graph(history)
+result = agent.invoke_graph(args.prompt)
